f6bd8b6c8e5f7d0f7efb661ea1b0637324e6bb5f
Joshua Evans <joshuaevans2003123@gmail.com>
2020-12-29 15:41:08 -0700

    creation

---
+++ titleSrapper.py
@@ -0,0 +1,44 @@
+import praw
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+
+def main():
+    reddit = praw.Reddit(username = 'SnekBotReddit',
+                    password = 'joshuaevans2003!',
+                    client_id = 'eiG17bKMV--z4Q',
+                    client_secret = 'n1bz222d6RT7Ac-N3SIKpjB6Slo5rA',
+                    user_agent = 'SnekBot by /u/SnekBotReddit -- https://github.com/JoshVEvans/RedditBot')
+
+    #####
+    subreddit = reddit.subreddit('all')
+    key_words = ['give away', 'giving away', 'giveaway']
+    
+    for submission in subreddit.stream.submissions():
+        for word in key_words:
+            if word in submission.title.lower(): #lowercase all submission titles:
+                print(f"Title: {submission.title}")
+                print(f"Url: reddit.com{submission.permalink}")
+                sendMail(f"reddit.com{submission.permalink}")
+                print('--------------------------------------------------')
+
+def sendMail(msg):  
+    sender = 'joshbot2003123@gmail.com'
+    password = 'ClickedMoss5'
+    server = 'smtp.gmail.com'
+    port = 465
+
+    server = smtplib.SMTP_SSL(server, port)
+    server.login(sender, password)
+
+    message = MIMEMultipart()
+
+    message['From'] = sender
+    message['To'] = 'joshbot2003123@gmail.com'
+    message['Subject'] = 'RedditBotMail'
+
+    message.attach(MIMEText(msg, 'plain'))
+    server.send_message(message)
+
+main()
