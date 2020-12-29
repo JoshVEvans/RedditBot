38cae7b5d75af4c01a12f26da1bf44b0eeacdacb
Joshua Evans <joshuaevans2003123@gmail.com>
2020-12-29 16:08:53 -0700

    update

--- redditBot.py
+++ redditBot.py
@@ -12,7 +12,7 @@ def main():
                     user_agent = 'SnekBot by /u/SnekBotReddit -- https://github.com/JoshVEvans/RedditBot')
 
     subreddit = reddit.subreddit('all')
-    key_words = ['give away', 'giving away', 'giveaway', 'gift']
+    key_words = ['give away', 'giving away', 'giveaway']
     
     for submission in subreddit.stream.submissions():
         for word in key_words:
