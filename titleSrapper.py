bd48c4a5a5a8580563c853464ad75e5ccbff1276
Joshua Evans <joshuaevans2003123@gmail.com>
2020-12-29 16:05:01 -0700

    final version, currently only tracking 3 versions of give away

--- titleSrapper.py
+++ titleSrapper.py
@@ -11,34 +11,31 @@ def main():
                     client_secret = 'n1bz222d6RT7Ac-N3SIKpjB6Slo5rA',
                     user_agent = 'SnekBot by /u/SnekBotReddit -- https://github.com/JoshVEvans/RedditBot')
 
-    #####
     subreddit = reddit.subreddit('all')
     key_words = ['give away', 'giving away', 'giveaway']
     
     for submission in subreddit.stream.submissions():
         for word in key_words:
             if word in submission.title.lower(): #lowercase all submission titles:
-                print(f"Title: {submission.title}")
-                print(f"Url: reddit.com{submission.permalink}")
-                sendMail(f"reddit.com{submission.permalink}")
+                print(f"Title: {submission.title} \nUrl: reddit.com{submission.permalink}")
+                
+                sendMail(f"{submission.title}",
+                         "reddit.com{submission.permalink}")
                 print('--------------------------------------------------')
 
-def sendMail(msg):
-    sender = 'SnekRedditBot@gmail.com'
-    password = 'pythonbot'
-    server = 'smtp.gmail.com'
-    port = 465
+
+def sendMail(subject, msg):
+    sender, password, server, port = 'snekredditbot@gmail.com', 'pythonbot', 'smtp.gmail.com', 465
 
     server = smtplib.SMTP_SSL(server, port)
     server.login(sender, password)
 
     message = MIMEMultipart()
-
-    message['From'] = sender
-    message['To'] = 'joshbot2003123@gmail.com'
-    message['Subject'] = 'RedditBotMail'
+    message['From'], message['To'], message['Subject'] = sender, 'snekredditbot@gmail.com', subject
 
     message.attach(MIMEText(msg, 'plain'))
     server.send_message(message)
+
+
 
 main()
